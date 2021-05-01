2949592d53c42fe9ecc05e259c11f9220ef742b4
neminem1203 <tpaul1203@gmail.com>
2021-04-30 22:27:12 -0400

    updated css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ProfilePicture from "./profile_picture";
 
 function App() {
   document.title = "Manifest";
@@ -8,15 +9,7 @@ function App() {
         <div><b>Manifest</b></div>
       </div>
       <div className="body">
-        <div className="profile-picture">
-          <img src="./61.png" alt="Henry" style={{width: "100%"}}/>
-          <div className="profile-picture-title">
-            Henry
-          </div>
-          <div className="profile-picture-subtitle">
-            Transfer Specialist
-          </div>
-        </div>
+        <ProfilePicture />
       </div>
       <div className="response-area">
         <div className="response">Response1</div>
---
+++ src/profile_picture.js
@@ -0,0 +1,14 @@
+
+const ProfilePicture = ()=>(
+    <div className="profile-picture">
+        <img src="./61.png" alt="Henry" style={{width: "100%"}}/>
+        <div className="profile-picture-title">
+            Henry
+        </div>
+        <div className="profile-picture-subtitle">
+            Transfer Specialist
+        </div>
+    </div>
+)
+
+export default ProfilePicture;
